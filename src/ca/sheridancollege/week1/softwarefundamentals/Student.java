3070962fe99298c6983f0f30f729ff67bd71a160
leenaabbi <abbil@sheridancollege.ca>
2020-02-12 10:22:11 -0500

    2st commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -11,7 +11,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
  */
 public class Student {
     
-    private status;
+    private String status;
 
     public int getId() {
         return id;
