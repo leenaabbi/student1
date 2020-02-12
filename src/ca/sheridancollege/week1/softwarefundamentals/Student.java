2e9f74790e12a3e805e95e0763e3bef60aafd45b
leenaabbi <59924418+leenaabbi@users.noreply.github.com>
2020-02-12 10:37:08 -0500

    Update Student.java

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -12,7 +12,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
 public class Student {
     
     private String status;
-   //new;
+   //old;
 
     public int getId() {
         return id;
